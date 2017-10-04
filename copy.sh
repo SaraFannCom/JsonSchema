21e45512c499255a50e96b95acd2aaece4978574
kaizer <kaizer@kai-zer.ru>
2017-10-04 14:30:37 +0800

    Feed; Wall; copy.sh

---
+++ copy.sh
@@ -0,0 +1,4 @@
+rm -rf methods
+cp  ../PhpstormProjects/Laravel/public/json/schema/methods.json .
+cp  ../PhpstormProjects/Laravel/public/json/schema/objects.json .
+cp -rf  ../PhpstormProjects/Laravel/public/json/schema/methods .
